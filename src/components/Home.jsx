2a412b590512e0f61a476a13d1f80cd217e2533b
Omega <elbackenddev22@gmail.com>
2025-07-19 22:11:35 -0600

    Nueva sección

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
 import '../css/Home.css';
 import ChinitaImg from '../assets/chinita.png';
 import Sun from '../assets/sun.png'
-import DemonSlayer from '../assets/demon_slayer.png';
+import CodeServiceUno from '../assets/code_service_1.png'
+import TesterServiceDos from '../assets/tester_service_2.png'
+import ClasesServiceDos from '../assets/clases_service_3.png'
+import RecursosServiceDos from '../assets/recursos_service_4.png'
 
 function Home() {
 
@@ -21,6 +24,37 @@ function Home() {
 
         <img src={ ChinitaImg } alt="chineseimg" className='chinese__img' />
       </div>
+
+      <section className='container__section__services'>
+        <h1 className='text__services'>¿Qué te ofrecemos en esta comunidad?</h1>
+        <article>
+          <div className="container__service__div">
+            <img src={ CodeServiceUno } alt="code__service" className='image__service__1' />
+          </div>
+          <h2 className='text__service__1'>Te ayudamos con los <br /> errores en tu código.</h2>
+        </article>
+        
+        <article>
+          <h2 className='text__service__2'>Testeamos tu <br /> proyecto con prácticas <br /> profesionales.</h2>
+          <div className="container__service__div">
+            <img src={ TesterServiceDos } alt="tester__service" className='image__service__1' />
+          </div>
+        </article>
+
+        <article>
+          <div className="container__service__div">
+            <img src={ ClasesServiceDos } alt="clases__service" className='image__service__1' />
+          </div>
+          <h2 className='text__service__3'>Asiste a las clases <br /> gratuitas de categoría <br /> técnica.</h2>
+        </article>
+
+        <article>
+          <h2 className='text__service__2'>Adquiere recursos de <br /> categoría premium <br /> para tu aprendizaje. </h2>
+          <div className="container__service__div">
+            <img src={ RecursosServiceDos } alt="tester__service" className='image__service__4' />
+          </div>
+        </article>
+      </section>
     </div>
   )
 
